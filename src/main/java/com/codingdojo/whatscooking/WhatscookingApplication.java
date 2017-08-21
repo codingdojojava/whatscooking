15a958c7d1ecc4c72755db53f8dc010f65162e2f
Artemio Cabelin <artemiocabelin@gmail.com>
2017-08-21 15:04:51 -0700

    Initial commit

---
+++ src/main/java/com/codingdojo/whatscooking/WhatscookingApplication.java
@@ -0,0 +1,12 @@
+package com.codingdojo.whatscooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatscookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatscookingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codingdojo/whatscooking/WhatscookingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codingdojo.whatscooking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhatscookingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
