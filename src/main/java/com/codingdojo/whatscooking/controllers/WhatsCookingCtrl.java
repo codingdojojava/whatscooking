06384264d41de036941a0d02ff0a2ca5452db59b
Rommel Castro <Rommel@Rommels-MacBook-Pro-2.local>
2017-08-25 12:10:38 -0700

    fixed carousel and rm fav/groc

--- src/main/java/com/codingdojo/whatscooking/controllers/WhatsCookingCtrl.java
+++ src/main/java/com/codingdojo/whatscooking/controllers/WhatsCookingCtrl.java
@@ -357,10 +357,11 @@ public class WhatsCookingCtrl {
 		return "showgroc";
 	}
 	@RequestMapping("/home/favorites/{favorite-id}/delete")
-	public String removeFavorite(@PathVariable(value="favorite-id") Long id, Principal principal, Model model) {
+	public String removeFavorite(@PathVariable(value="favorite-id") String id, Principal principal, Model model) {
+		System.out.println("PLSSSFASDFASDFASFAS");
 		String username = principal.getName();
 		User user = whatsCookingServices.findByUsername(username);
-		Recipe recipe = recipeServ.getRById(id);
+		Recipe recipe = recipeServ.getByName(id);
 		whatsCookingServices.removeRecipeFromFavorites(user, recipe);
 		model.addAttribute("favoritess", user.getFavorites());
 		return "showfavs";
